d1a04236cb1e8b2de676276f204f5fd8159b1a4f
qzlu-cyber <941648981@qq.com>
2023-12-05 18:23:37 +0800

    Add Pickup class

---
+++ Source/Blaster/Pickups/Pickup.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pickup.h"
+#include "Blaster/Weapon/WeaponTypes.h"
+
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+
+APickup::APickup()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	bReplicates = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	OverlapSphere = CreateDefaultSubobject<USphereComponent>(TEXT("OverlapSphere"));
+	OverlapSphere->SetupAttachment(RootComponent);
+	OverlapSphere->SetSphereRadius(50.f);
+	OverlapSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly); // 只检测碰撞
+	OverlapSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap); // 只检测 Pawn
+
+	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Pickup Mesh"));
+	PickupMesh->SetupAttachment(RootComponent);
+	PickupMesh->SetRenderCustomDepth(true);
+	PickupMesh->SetCustomDepthStencilValue(CUSTOM_DEPTH_PURPLE);
+	PickupMesh->MarkRenderStateDirty();
+}
+
+void APickup::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority()) OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnSphereOverlap);
+}
+
+void APickup::OnSphereOverlap(UPrimitiveComponent* OverlappedSphere, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void APickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APickup::Destroyed()
+{
+	Super::Destroyed();
+
+	if (PickupSound) UGameplayStatics::PlaySoundAtLocation(this, PickupSound, GetActorLocation());
+}
---
+++ Source/Blaster/Pickups/Pickup.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+UCLASS()
+class BLASTER_API APickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	APickup();
+
+	virtual void Destroyed() override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedSphere,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	class USphereComponent* OverlapSphere;
+	
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* PickupMesh;
+	
+	UPROPERTY(EditAnywhere)
+	class USoundCue* PickupSound;
+};
