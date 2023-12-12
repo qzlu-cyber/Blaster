85b9c4a3b4471f82e8cefeb6c37f775f8a9079b6
qzlu-cyber <941648981@qq.com>
2023-12-12 16:31:05 +0800

    Add LagCompensationComponent

---
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
@@ -0,0 +1,16 @@
+#include "LagCompensationComponent.h"
+
+ULagCompensationComponent::ULagCompensationComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void ULagCompensationComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
